a413f6cc8fcb53c760cba7a4fff4564695c83388
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-03 22:55:54 -0500

    secrect page logic

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 
 import {createRouter, createWebHistory} from "vue-router";
+import useAuth from "./composable/useAuth";
 
 import About from "./pages/about.vue";
 import Login from "./pages/login.vue";
 import Index from "./pages/index.vue";
+import Secret from "./pages/secret.vue";
 import notFound from "./pages/404.vue";
+
+const {isAuthenticated} = useAuth();
+
 
 
 const routes = [
@@ -22,6 +27,18 @@ const routes = [
         path: "/login",
         name: "Login",
         component: Login,
+    },
+    {
+        path: "/secret",
+        name: "Secret",
+        component: Secret,
+        beforeEnter: (to, from, next) => {
+            if (!isAuthenticated.value){
+               next("login");
+            }
+            next();
+            
+        },
     },
     {
         path: "/:pathMatch(.*)*",
